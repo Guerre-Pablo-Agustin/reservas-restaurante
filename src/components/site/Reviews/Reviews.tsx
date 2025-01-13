3f1bc572b916e564c92401628ddc2a0d0a482215
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2025-01-13 16:26:55 -0300

    removidas la imagenes de las reviews

--- src/components/site/Reviews/Reviews.tsx
+++ src/components/site/Reviews/Reviews.tsx
@@ -1,74 +1,79 @@
-'use client'
-import { reviews } from './data.reviews'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Autoplay, Pagination } from 'swiper/modules'
-import { FaStar } from 'react-icons/fa'
-import Image from 'next/image'
+"use client";
+import { reviews } from "./data.reviews";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import { FaStar } from "react-icons/fa";
 
 // Importar estilos de Swiper
-import 'swiper/css'
-import 'swiper/css/pagination'
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Reviews = () => {
-    return (
-        <section className="py-16 bg-gray-50" id="reviews">
-            <div className="container mx-auto px-4">
-                <div className="text-center mb-12">
-                    <h2 className="text-4xl font-bold text-gray-800 mb-4">Lo que dicen nuestros clientes</h2>
-                    <p className="text-gray-600">Experiencias compartidas por quienes nos han visitado</p>
-                </div>
-                
-                <Swiper
-                    modules={[Autoplay, Pagination]}
-                    spaceBetween={30}
-                    slidesPerView={1}
-                    pagination={{ clickable: true }}
-                    autoplay={{
-                        delay: 5000,
-                        disableOnInteraction: false,
-                    }}
-                    breakpoints={{
-                        640: {
-                            slidesPerView: 1,
-                        },
-                        768: {
-                            slidesPerView: 2,
-                        },
-                        1024: {
-                            slidesPerView: 3,
-                        },
-                    }}
-                    className="py-8"
-                >
-                    {reviews.map((review) => (
-                        <SwiperSlide key={review.id}>
-                            <div className="bg-white p-6 rounded-lg shadow-lg">
-                                <div className="flex items-center mb-4">
-                                    <div>
-                                        <h3 className="font-semibold text-lg">{review.name}</h3>
-                                        <div className="flex">
-                                            {[...Array(5)].map((_, index) => (
-                                                <FaStar
-                                                    key={index}
-                                                    className={`w-4 h-4 ${
-                                                        index < review.rating
-                                                            ? 'text-yellow-400'
-                                                            : 'text-gray-300'
-                                                    }`}
-                                                />
-                                            ))}
-                                        </div>
-                                    </div>
-                                </div>
-                                <p className="text-gray-600 italic">&quot;{review.comment}&quot;</p>
-                                <p className="text-sm text-gray-500 mt-2">{review.date}</p>
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="bg-gray-50 py-16" id="reviews">
+      <div className="container mx-auto px-4">
+        <div className="mb-12 text-center">
+          <h2 className="mb-4 text-4xl font-bold text-gray-800">
+            Lo que dicen nuestros clientes
+          </h2>
+          <p className="text-gray-600">
+            Experiencias compartidas por quienes nos han visitado
+          </p>
+        </div>
 
-export default Reviews
+        <Swiper
+          modules={[Autoplay, Pagination]}
+          spaceBetween={30}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+          className="py-8"
+        >
+          {reviews.map((review) => (
+            <SwiperSlide key={review.id}>
+              <div className="rounded-lg bg-white p-6 shadow-lg">
+                <div className="mb-4 flex items-center">
+                  <div>
+                    <h3 className="text-lg font-semibold">{review.name}</h3>
+                    <div className="flex">
+                      {[...Array(5)].map((_, index) => (
+                        <FaStar
+                          key={index}
+                          className={`h-4 w-4 ${
+                            index < review.rating
+                              ? "text-yellow-400"
+                              : "text-gray-300"
+                          }`}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <p className="italic text-gray-600">
+                  &quot;{review.comment}&quot;
+                </p>
+                <p className="mt-2 text-sm text-gray-500">{review.date}</p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default Reviews;

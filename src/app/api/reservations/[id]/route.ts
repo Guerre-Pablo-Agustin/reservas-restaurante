4663a5c70a23da18e6e2b9afa2787812f7378819
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2025-01-16 18:33:08 -0300

    cambios en la ruta get de reservas

--- src/app/api/reservations/[id]/route.ts
+++ src/app/api/reservations/[id]/route.ts
@@ -1,33 +1,28 @@
-import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma"; 
+import { NextResponse, NextRequest } from "next/server";
+import prisma from "@/lib/prisma";
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
     const { id } = params;
     try {
-        const reservation = await prisma.reservation.delete({ where: { id: id } }); 
-        return NextResponse.json({ mensaje: "Reserva eliminada", reservation: reservation }); 
+        const reservation = await prisma.reservation.delete({ where: { id: id } });
+        return NextResponse.json({ mensaje: "Reserva eliminada", reservation: reservation });
     } catch (error) {
-        return NextResponse.json({ mensaje: "Error al eliminar la reserva", error: error }); 
+        return NextResponse.json({ mensaje: "Error al eliminar la reserva", error: error });
     }
 }
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
     const { id } = params;
     try {
-        const reservation = await prisma.reservation.findUnique({
-            where: { id: id },
-        });
-
+        const reservation = await prisma.reservation.findUnique({ where: { id: id } });
         if (!reservation) {
             return NextResponse.json({ mensaje: "Reserva no encontrada" }, { status: 404 });
         }
-
-        return NextResponse.json({ mensaje: "Reserva obtenida", reservation: reservation });
+        return NextResponse.json(reservation);
     } catch (error) {
-        return NextResponse.json({ mensaje: "Error al obtener la reserva", error: error }); 
+        return NextResponse.json({ mensaje: "Error al obtener la reserva", error: error });
     }
 }
-
 export async function PUT(request: Request, { params }: { params: { id: string } }) {
     const { id } = params;
     const { clientName, details, status, date, time, quantity } = await request.json(); 

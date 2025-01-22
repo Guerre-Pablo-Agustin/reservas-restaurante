24ddcbeaeec34017d237c8d6a85261d2d6731ec6
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2025-01-22 17:06:49 -0300

    ajustes en parametro de pagina edit reserva

--- src/app/dashboard/[id]/edit/page.tsx
+++ src/app/dashboard/[id]/edit/page.tsx
@@ -1,14 +1,21 @@
+"use client";
 import Details from "@/components/Dashboard/Home/Details";
+import { useParams } from "next/navigation";
 import React from "react";
 
-const page = ({ params }: { params: { id: string } }) => {
-  console.log("params", params?.id);
+
+
+const Page = () => {
+  const router = useParams();
+  const { id } = router;
+
+  console.log("params", id);
 
   return (
     <div>
-      <Details selectId={params?.id} />
+      <Details selectId={id as string} />
     </div>
   );
 };
 
-export default page;
+export default Page;
